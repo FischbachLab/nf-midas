9bca223e033642f4a98e5f860dd59fbabac9bdbd
Sunit Jain <mail4sunit@gmail.com>
2022-02-23 14:34:30 -0800

    fixed path to midas db

--- run_job.sh
+++ run_job.sh
@@ -4,7 +4,7 @@ aws batch submit-job \
     --job-queue priority-maf-pipelines \
     --job-definition nextflow-production \
     --container-overrides command=FischbachLab/nf-midas,\
-"--db_midas","s3://dev-scratch/ReferenceDBs/Midas/v1.2/midas_db_v1.2.tar.gz",\
+"--db_midas","s3://dev-scratch/ReferenceDBs/Midas/v1.2",\
 "--db_knead","s3://dev-scratch/ReferenceDBs/Biobakery/kneaddata/human_genome/bowtie2",\
 "--output_folder","s3://genomics-workflow-core/Results/midas/hCom-B6Gen",\
 "--manifest","s3://genomics-workflow-core/Results/midas/hCom-B6Gen/00_seedfile/hCom-B6Gen.seedfile.csv"
