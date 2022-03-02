d31642c31460b16287490965efdcde23d297168c
Sunit Jain <mail4sunit@gmail.com>
2022-03-02 11:02:41 -0800

    removed kneaddata process

--- run_job.sh
+++ run_job.sh
@@ -1,10 +1,16 @@
-# Paired end
+# TEST
 aws batch submit-job \
-    --job-name nf-midas-0223-1 \
+    --job-name nf-midas-0225-1 \
     --job-queue priority-maf-pipelines \
     --job-definition nextflow-production \
     --container-overrides command=FischbachLab/nf-midas,\
-"--db_midas","s3://dev-scratch/ReferenceDBs/Midas/v1.2",\
-"--db_knead","s3://dev-scratch/ReferenceDBs/Biobakery/kneaddata/human_genome/bowtie2",\
-"--output_folder","s3://genomics-workflow-core/Results/midas/hCom-B6Gen",\
+"--manifest","s3://genomics-workflow-core/Results/midas/00_TEST/00_seedfile/test.seedfile.csv"
+
+# Paired end
+aws batch submit-job \
+    --job-name nf-midas-0224-1 \
+    --job-queue priority-maf-pipelines \
+    --job-definition nextflow-production \
+    --container-overrides command=FischbachLab/nf-midas,\
+"--project","hCom-B6Gen",\
 "--manifest","s3://genomics-workflow-core/Results/midas/hCom-B6Gen/00_seedfile/hCom-B6Gen.seedfile.csv"
